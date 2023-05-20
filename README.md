2942ec7983c0604d28061acee221c2c17afbb822
Mark Munkacsi <markmunkacsi@protonmail.com>
2023-05-20 17:07:13 +0400

    add umami and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# SA EV Loot checker
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/6c222db4-9b9a-42e4-846b-cc040d6f8059/deploy-status)](https://app.netlify.com/sites/staratlas-ev-reward-checker/deploys)
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+const umamiWebsiteId: string = import.meta.env.VITE_UMAMI_WEBSITE_ID;
+if (umamiWebsiteId) {
+  const umamiScript = document.createElement("script");
+  umamiScript.src = "https://analytics.umami.is/script.js";
+  umamiScript.async = true;
+  umamiScript.setAttribute("data-website-id", umamiWebsiteId);
+  document.head.appendChild(umamiScript);
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
